bb7a20e6f1feee169df9ccd84138adfbf1943983
lonewolfyx <877085893@qq.com>
2025-12-25 23:11:47 +0800

    chore: Disable typescript-related rules for ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,10 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
     // Your custom configs here
+    {
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+        },
+    },
 )
