1dfe8b075f37c1c80db7f4234bb8e6c6dd9f74e5
lonewolfyx <877085893@qq.com>
2025-12-26 16:52:00 +0800

    feat(github): Dynamic rendering and data binding for a list of popular repositories

--- app/components/github/GithubTopRepository.vue
+++ app/components/github/GithubTopRepository.vue
@@ -11,16 +11,22 @@
         <CardContent class="p-2">
             <!--            今年热门的仓库,按照 star、fork 排序 -->
             <div class="flex flex-col gap-2">
-                <GithubTopRepositoryItems />
-                <GithubTopRepositoryItems />
-                <GithubTopRepositoryItems />
+                <GithubTopRepositoryItems
+                    v-for="item in repository.repositories.nodes.slice(0, 3)"
+                    :key="item.name"
+                    :repository="item"
+                />
             </div>
         </CardContent>
     </SiteCard>
 </template>
 
 <script lang="ts" setup>
+import { useGithubData } from '~/components/github/index'
+
 defineOptions({
     name: 'GithubTopRepository',
 })
+
+const { repository } = useGithubData()
 </script>
--- app/components/github/GithubTopRepositoryItems.vue
+++ app/components/github/GithubTopRepositoryItems.vue
@@ -1,42 +1,53 @@
 <template>
-    <div class="bg-neutral-700/50 border border-neutral-700 rounded-sm p-2 space-y-2">
+    <NuxtLink
+        :to="repository.url"
+        class="bg-neutral-700/50 border border-neutral-700 rounded-sm p-2 space-y-2"
+        target="_blank"
+    >
         <h3 class="base-text text-sm md:text-base">
-            nuxt
+            {{ repository.name }}
         </h3>
         <p class="text-muted/50 text-xs md:text-sm line-clamp-1">
-            The Progressive Web Framework.The Progressive Web Framework.The Progressive Web Framework.The
-            Progressive Web Framework.The Progressive Web Framework.
+            {{ repository.description }}
         </p>
         <div class="flex items-center gap-4">
             <div class="flex items-center gap-1">
                 <div
+                    :style="{
+                        backgroundColor: repository.primaryLanguage.color,
+                    }"
                     class="w-2 h-2 rounded-full"
-                    style="background-color: rgb(49, 120, 198);"
                 />
-                <span class="text-sm text-muted-foreground">TypeScript</span>
+                <span class="text-sm text-muted-foreground">{{ repository.primaryLanguage.name }}</span>
             </div>
             <div class="flex items-center gap-1">
                 <Icon
+                    class="text-stone-300"
+                    mode="svg"
                     name="solar:star-linear"
-                    mode="svg"
-                    class="text-stone-300"
                 />
-                <span class="text-sm text-muted-foreground">99999999</span>
+                <span class="text-sm text-muted-foreground">{{ repository.stargazerCount }}</span>
             </div>
             <div class="flex items-center gap-1">
                 <Icon
-                    name="lucide:git-fork"
-                    mode="svg"
                     class="text-stone-300"
+                    mode="svg"
+                    name="lucide:git-fork"
                 />
-                <span class="text-sm text-muted-foreground">99999999</span>
+                <span class="text-sm text-muted-foreground">{{ repository.forkCount }}</span>
             </div>
         </div>
-    </div>
+    </NuxtLink>
 </template>
 
 <script lang="ts" setup>
+import type { IRepository } from '~/types/github'
+
 defineOptions({
     name: 'GithubTopRepositoryItems',
 })
+
+const props = defineProps<{
+    repository: IRepository
+}>()
 </script>
