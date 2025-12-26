ca5696c8ff41375373ed47c840763232a9e10f03
lonewolfyx <877085893@qq.com>
2025-12-26 17:09:05 +0800

    feat(footer): Converts footer author links into clickable navigation links

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -2,7 +2,10 @@
     <div class="border-t border-neutral-700 text-neutral-600 py-2">
         <div class="flex justify-between items-center text-xs">
             <span>&copy; Github Wrapped 2025</span>
-            <span>Made with by ♥️ lonewolfyx</span>
+            <span>Made with by ♥️ <NuxtLink
+                target="_blank"
+                to="https://github.com/lonewolfyx"
+            >lonewolfyx</NuxtLink></span>
         </div>
     </div>
 </template>
