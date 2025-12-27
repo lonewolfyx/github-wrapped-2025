53a16fa493d28f3313ebee05980abd834ca64a15
lonewolfyx <877085893@qq.com>
2025-12-27 10:25:15 +0800

    style(github): Simplify property declarations for div containers

--- app/pages/[[user]].vue
+++ app/pages/[[user]].vue
@@ -2,9 +2,7 @@
     <Suspense>
         <template #default>
             <GithubContainer>
-                <div
-                    class="flex h-full flex-col space-y-4 pb-4"
-                >
+                <div class="flex h-full flex-col space-y-4 pb-4">
                     <SiteHeader />
                     <div class="flex-1 overflow-hidden">
                         <div class="grid h-full grid-cols-1 md:grid-cols-3 grid-rows-4 gap-4">
